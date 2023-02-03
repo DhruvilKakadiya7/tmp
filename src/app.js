4db6b5329b6c1d88b6d55a9059236d2078b53c11
DhruvilKakadiya7 <kakadiyadhruvil3006@gmail.com>
2023-02-03 18:46:29 +0530

    chenges in app.js and added after login page

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ app.post('/',async (req,res)=>{
         if(userEmail){
             if(matching){
                 console.log("LOGIN SUCCESSFULL");
-                res.status(201).redirect("login");
+                res.status(201).render("login");
             }
             else{
                 res.status(400).send("Incorrect Email or password");
