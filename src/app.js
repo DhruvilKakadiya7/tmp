fda6e884abd41418bd161eb3b4ad5555a33c792b
DhruvilKakadiya7 <kakadiyadhruvil3006@gmail.com>
2023-02-04 00:28:53 +0530

    cookies added

--- src/app.js
+++ src/app.js
@@ -38,7 +38,6 @@ app.post('/',async (req,res)=>{
         const matching = await bcrypt.compare(req.body.pass,userEmail.password);
         const token = await userEmail.generateAuthToken();
         res.cookie("jwt",token);
-        // console.log("cookie" + document.cookie);
         console.log(matching);
         if(userEmail){
             if(matching){
@@ -67,7 +66,6 @@ app.post('/index',async (req,res)=>{
         const matching = await bcrypt.compare(req.body.pass,userEmail.password);
         const token = await userEmail.generateAuthToken();
         res.cookie("jwt",token);
-        // console.log("cookie" + document.cookie);
         console.log(matching);
         if(userEmail){
             if(matching){
